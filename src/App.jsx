6a65882055160036c414aff3830201908b329502
Konstantin <117026423+Kohstantih@users.noreply.github.com>
2024-01-10 15:32:34 +0300

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,45 @@
-import { useState } from 'react'
 import './App.css'
+
+const item = {
+  brand: 'Tiger of Sweden',
+  title: 'Leonard coat',
+  description: 'Minimalistic coat in cotton-blend',
+  descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
+  price: 399,
+  currency: '£'
+}
+
+function ShopItemFunc (props) {
+  return (
+    <div class="main-content">
+      <h2>{props.item.brand}</h2>
+      <h1>{props.item.title}</h1>
+      <h3>{props.item.description}</h3>
+      <div class="description">
+        {props.item.descriptionFull}
+      </div>
+      <div class="highlight-window mobile"><div class="highlight-overlay"></div></div>
+      <div class="divider"></div>
+      <div class="purchase-info">
+        <div class="price">{props.item.currency}{props.item.price}.00</div>
+        <button>Добавить в корзину</button>
+      </div>
+    </div>
+  )
+}
 
 function App() {
   return (
     <>
-      
+      <div className="container">
+        <div className="background-element"></div>
+        <div className="highlight-window">
+          <div className='highlight-overlay'></div>
+        </div>
+        <div className="window">
+          <ShopItemFunc item={item} />
+        </div>
+      </div>
     </>
   )
 }
