773c8b1582a035cd884da7649321f388b77832c0
Konstantin <117026423+Kohstantih@users.noreply.github.com>
2024-01-10 16:07:19 +0300

    fixed className

--- src/App.jsx
+++ src/App.jsx
@@ -11,17 +11,17 @@ const item = {
 
 function ShopItemFunc (props) {
   return (
-    <div class="main-content">
+    <div className="main-content">
       <h2>{props.item.brand}</h2>
       <h1>{props.item.title}</h1>
       <h3>{props.item.description}</h3>
-      <div class="description">
+      <div className="description">
         {props.item.descriptionFull}
       </div>
-      <div class="highlight-window mobile"><div class="highlight-overlay"></div></div>
-      <div class="divider"></div>
-      <div class="purchase-info">
-        <div class="price">{props.item.currency}{props.item.price}.00</div>
+      <div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
+      <div className="divider"></div>
+      <div className="purchase-info">
+        <div className="price">{props.item.currency}{props.item.price}.00</div>
         <button>Добавить в корзину</button>
       </div>
     </div>
